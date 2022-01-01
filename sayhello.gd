2323cec2b0dac032ffbc6b262c815e39829c1c8c
Igor.Dorozhkin <Igor.Dorozhkin@nedra.digital>
2022-01-01 14:42:30 +0300

    testTree

--- sayhello.gd
+++ sayhello.gd
@@ -1,17 +1,18 @@
 extends Panel
 
-#var accum = 0
-#
-#func _process(delta):
-#	accum += delta
-#	get_node("Counter").text = str(accum) # 'text' is a built-in label property.
+var accum = 0
+#var commons = get_tree().get_nodes_in_group("Commons")
+
+func _process(delta):
+	accum += delta
+	get_node("Counter").text = str(accum) # 'text' is a built-in label property.
 
 func _ready():
-	add_to_group("Common")
 	get_node("Button").connect("button_down", self, "_on_Button_button_down")
 	get_node("Button").connect("button_up", self, "_on_Button_button_up")
 
 func _on_Button_button_down():
+#	get_node("Label").text = str(commons)
 	get_node("Label").text = "Hello!"
 
 func _on_Button_button_up():
