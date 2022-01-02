d39fe6789ae61e0df0fcb2ad6a170fc051d62f04
Igor.Dorozhkin <Igor.Dorozhkin@nedra.digital>
2022-01-02 11:54:19 +0300

    commit before game

--- sayhello.gd
+++ sayhello.gd
@@ -1,19 +1,16 @@
 extends Panel
 
-var accum = 0
-#var commons = get_tree().get_nodes_in_group("Commons")
-
-func _process(delta):
-	accum += delta
-	get_node("Counter").text = str(accum) # 'text' is a built-in label property.
-
 func _ready():
 	get_node("Button").connect("button_down", self, "_on_Button_button_down")
 	get_node("Button").connect("button_up", self, "_on_Button_button_up")
+	$Timer.connect("timeout", self, "_on_Timer_timeout")
 
 func _on_Button_button_down():
-#	get_node("Label").text = str(commons)
 	get_node("Label").text = "Hello!"
 
 func _on_Button_button_up():
 	get_node("Label").text = ""
+
+
+func _on_Timer_timeout():
+	$Sprite.visible = !$Sprite.visible
