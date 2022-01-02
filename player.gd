477d67e21608879f30f8cbc8b1a4e0635f95aa5f
Igor.Dorozhkin <Igor.Dorozhkin@nedra.digital>
2022-01-02 14:57:52 +0300

    Preparing for collisions

---
+++ player.gd
@@ -0,0 +1,44 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+export var speed = 400  # How fast the player will move (pixels/sec).
+var screen_size  # Size of the game window.
+
+
+# The _ready() function is called when a node enters the scene tree,
+# which is a good time to find the size of the game window:
+func _ready():
+	screen_size = get_viewport_rect().size
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+		var velocity = Vector2()  # The player's movement vector.
+		if Input.is_action_pressed("ui_right"):
+				velocity.x += 1
+		if Input.is_action_pressed("ui_left"):
+				velocity.x -= 1
+		if Input.is_action_pressed("ui_down"):
+				velocity.y += 1
+		if Input.is_action_pressed("ui_up"):
+				velocity.y -= 1
+		if velocity.length() > 0:
+				velocity = velocity.normalized() * speed
+				$AnimatedSprite.play()
+		else:
+				$AnimatedSprite.stop()
+				
+# We can also use clamp() to prevent player from leaving the screen		
+		position += velocity * delta
+		position.x = clamp(position.x, 0, screen_size.x)
+		position.y = clamp(position.y, 0, screen_size.y)
+
+# Choosing animations
+		if velocity.x != 0:
+			$AnimatedSprite.animation = "walk"
+			$AnimatedSprite.flip_v = false
+			# See the note below about boolean assignment
+			$AnimatedSprite.flip_h = velocity.x < 0
+		elif velocity.y != 0:
+			$AnimatedSprite.animation = "up"
+			$AnimatedSprite.flip_v = velocity.y > 0
