001672826f3d7f9a86e99bf814d90d7aa8075887
Igor.Dorozhkin <Igor.Dorozhkin@nedra.digital>
2022-01-04 01:17:19 +0300

    add animation 4 mobs, gui and sound fix

--- player.gd
+++ player.gd
@@ -10,7 +10,7 @@ var target = Vector2()
 # The _ready() function is called when a node enters the scene tree,
 # which is a good time to find the size of the game window:
 func _ready():
-	hide() # Player disappears after being hit.
+	hide()
 	screen_size = get_viewport_rect().size
 
 func start(pos):
@@ -22,43 +22,43 @@ func start(pos):
 	
 # Change the target whenever a touch event happens.
 func _input(event):
-		if event is InputEventScreenTouch and event.pressed:
-				target = event.position
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-		var velocity = Vector2()  # The player's movement vector.
-		# Move towards the target and stop when close.
-		if position.distance_to(target) > 10:
-			velocity = target - position
-#		# Remove keyboard controls.
-#		if Input.is_action_pressed("ui_right"):
-#				velocity.x += 1
-#		if Input.is_action_pressed("ui_left"):
-#				velocity.x -= 1
-#		if Input.is_action_pressed("ui_down"):
-#				velocity.y += 1
-#		if Input.is_action_pressed("ui_up"):
-#				velocity.y -= 1
-		if velocity.length() > 0:
-				velocity = velocity.normalized() * speed
-				$AnimatedSprite.play()
-		else:
-				$AnimatedSprite.stop()
+	var velocity = Vector2()  # The player's movement vector.
+	#		# mouse controls. 
+	#		if position.distance_to(target) > 10:
+	#			velocity = target - position
+	# keyboard controls.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
 				
-		# We can also use clamp() to prevent player from leaving the screen		
-		position += velocity * delta
-		position.x = clamp(position.x, 0, screen_size.x)
-		position.y = clamp(position.y, 0, screen_size.y)
+	# We can also use clamp() to prevent player from leaving the screen		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
 
-		# Choosing animations
-		if velocity.x != 0:
-			$AnimatedSprite.animation = "walk"
-			$AnimatedSprite.flip_v = false
-			$AnimatedSprite.flip_h = velocity.x < 0
-		elif velocity.y != 0:
-			$AnimatedSprite.animation = "up"
-			$AnimatedSprite.flip_v = velocity.y > 0
+	# Choosing animations
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
 
 
 func _on_Player_body_entered(body):
