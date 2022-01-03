de5a5995fce77b94e769f9a79bd9726a53ed90bc
Igor.Dorozhkin <Igor.Dorozhkin@nedra.digital>
2022-01-03 21:56:56 +0300

    make apk

--- player.gd
+++ player.gd
@@ -4,24 +4,42 @@ signal hit
 # Declare member variables here. Examples:
 export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
-
+# Add this variable to hold the clicked position.
+var target = Vector2()
 
 # The _ready() function is called when a node enters the scene tree,
 # which is a good time to find the size of the game window:
 func _ready():
+	hide() # Player disappears after being hit.
 	screen_size = get_viewport_rect().size
+
+func start(pos):
+	position = pos
+	# Initial target is the start position.
+	target = pos
+	show()
+	$CollisionShape2D.disabled = false
+	
+# Change the target whenever a touch event happens.
+func _input(event):
+		if event is InputEventScreenTouch and event.pressed:
+				target = event.position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 		var velocity = Vector2()  # The player's movement vector.
-		if Input.is_action_pressed("ui_right"):
-				velocity.x += 1
-		if Input.is_action_pressed("ui_left"):
-				velocity.x -= 1
-		if Input.is_action_pressed("ui_down"):
-				velocity.y += 1
-		if Input.is_action_pressed("ui_up"):
-				velocity.y -= 1
+		# Move towards the target and stop when close.
+		if position.distance_to(target) > 10:
+			velocity = target - position
+#		# Remove keyboard controls.
+#		if Input.is_action_pressed("ui_right"):
+#				velocity.x += 1
+#		if Input.is_action_pressed("ui_left"):
+#				velocity.x -= 1
+#		if Input.is_action_pressed("ui_down"):
+#				velocity.y += 1
+#		if Input.is_action_pressed("ui_up"):
+#				velocity.y -= 1
 		if velocity.length() > 0:
 				velocity = velocity.normalized() * speed
 				$AnimatedSprite.play()
@@ -47,8 +65,3 @@ func _on_Player_body_entered(body):
 	hide()  # Player disappears after being hit.
 	emit_signal("hit")
 	$CollisionShape2D.set_deferred("disabled", true)
-
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
